cb164923b8f13ef2323cb521b345b6dd79bd9955
ณภัทร  พรบุญเรือง <42913846+61070044@users.noreply.github.com>
2019-03-10 23:45:51 +0700

    Update script.js

--- Web/script.js
+++ Web/script.js
@@ -87,4 +87,11 @@ function initGrid(){
 		}
 		grid.push(col);
 	}
+	// 1:03:06-1:24:51 from Tian line127+++
+	function onPath() {
+
+		for (var i = 0; i < path.lenght; i++)
+			if (path[i].x !== steps[i].x || path[i].y !== steps[i].y &&)
+				return false;
+	}
 }
