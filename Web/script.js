624b776ead5d91e42d19c563fc732cf31f470363
jfornqz <61070065@kmitl.ac.th>
2019-03-12 01:33:00 +0700

    update with some syntax error

--- Web/script.js
+++ Web/script.js
@@ -3,17 +3,25 @@ const HEIGHT = 5;
 
 var grid = [];
 var path = [];
-var step = [];
-var state;
+// var step = [];
+
+var size;
+
+var state; // 0 = animation and 1 = playing
 var animation;
 
 function setup() {
-	createCanvas(400,400);
+	createCanvas(400, 400);
+
+	size = Math.min(width / WIDTH, height / HEIGHT);
+
 	initGrid();
 	newPath()
 }
+
 function draw(){
 	background(51);
+
 	if (state === 0)
 		animate();
 	drawGrid();
@@ -22,33 +30,44 @@ function draw(){
 function drawGrid(){
 	strokeWeight(2);
 	stroke(255);
-	var size = Math.min(width/WIDTH,height/HEIGHT);
+
+	var size = Math.min(width / WIDTH, height / HEIGHT);
+
 	for (var x = 0; x < WIDTH; x++){
-		for (var y= 0; y < HEIGHT; y++) {
+		for (var y = 0; y < HEIGHT; y++) {
+
 			if (grid[x][y].lit)
-				fill(255);
+				fill(200);
 			else
 				noFill();
+
 			rect(x * size, y* size, size, size);
 		}
 	}
+
 }
+
 function animate(){
 	animation++;
 
 	var route = Math.floor(animation / 60);
 
-	if(route > path.length){
-		state = 1;
+	if(route >= path.length){
+
+		state = 1; //done animating
+		initGrid();
 		return;
 	}
+
+	console.log("flag");
+
 	var tile = path[route];
 	grid[tile.x][tile.y].lit = true;
 }
 
 function newPath() {
-	state = 0;
-	animation = 0;
+	state = 0; //animating
+	animation = 0; //time
 
 	path.push(new Tile(0, HEIGHT - 1, true));
 
@@ -62,36 +81,42 @@ function newPath() {
 		var right = new Tile(prevTile.x + 1, prevTile.y, true);
 		var up = new Tile(prevTile.x, prevTile.y - 1, true);
 
-		console.log(left.x);
-
-		if (left.x >= 0 && !pool.includes(left))
+		if (left.x >= 0 && !arrIncludes(path, left))
 			pool.push(left);
 
-		if (right.x <= WIDTH && !pool.includes(left))
+		if (right.x <= WIDTH && !arrIncludes(path, right))
 			pool.push(right);
 
-		if (up.y < HEIGHT && !pool.includes(up))
+		if (up.y < HEIGHT && !arrIncludes(path, up))
 			pool.push(up);
 
-		path.push(random(pool)):
-		i++;
+		path.push(random(pool));
 	}
 }
 
 function initGrid(){
-	var size = Math.min(width/WIDTH,height/HEIGHT);
+
+	grid= [];
+
 	for (var x = 0; x < WIDTH; x++){
 		var col = [];
 		for (var y= 0; y < HEIGHT; y++) {
+
 			col.push(new Tile(x, y, false));
 		}
 		grid.push(col);
 	}
-	// 1:03:06-1:24:51 from Tian line127+++
-	function onPath() {
 
-		for (var i = 0; i < path.lenght; i++)
-			if (path[i].x !== steps[i].x || path[i].y !== steps[i].y &&)
-				return false;
-	}
+};
+
+function arrIncludes(pool, tile) {
+
+	var t = JSON.stringify(tile);
+	for (var i = 0; i < pool.length; i++)
+		if (JSON.stringify(pool[i] === t)
+		return trues;
+
+	return false;
+};
+
 }
---
+++ Web/tian.js
@@ -0,0 +1,7 @@
+    // 1:03:06-1:24:51 from Tian line127+++
+function onPath() {
+
+        for (var i = 0; i < path.lenght; i++)
+            if (path[i].x !== steps[i].x || path[i].y !== steps[i].y &&)
+                return false;
+    }
