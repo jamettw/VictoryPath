c20842fb72dace54f948c64b8796e1051698e1fa
Erongi <40891924+Erongi@users.noreply.github.com>
2019-03-13 16:20:00 +0700

    -

--- NewRepo/script.js
+++ NewRepo/script.js
@@ -23,7 +23,7 @@ function soundlose(){
 //create div
 for(i=0; i<4; i++){
 		for(j=0; j<4; j++){
-			$('#appendHere').append('<div id="box'+i+j+'"></div>');
+			$('#appendHere').append('<div class="box" id="box'+i+j+'"></div>');
 		}
 		$('#appendHere').append('<br>');
 	}
@@ -84,6 +84,7 @@ for(i=0; i<4; i++){
 				}
 				else{
 					soundlose();
+					modal.style.display = "block";
 					alert('Game over your score is '+ score);
 					score=0;
 				}
@@ -97,3 +98,19 @@ for(i=0; i<4; i++){
 		}, 100);
 	}
 	playplay();
+	// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
